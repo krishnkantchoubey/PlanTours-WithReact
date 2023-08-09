3e7fb6e19283c8608363af09d8bae683d56b3b2a
Krishna kant <krishnakant3005@gmail.com>
2023-08-09 11:03:51 +0530

    fixed

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -5,7 +5,7 @@ function Tours({tours, removeTour}) {
             <div>
                 <h2 className='title'> Plan With Love</h2>
             </div>
-            <div className='card'>
+            <div className='cards'>
                 {
                     tours.map( (tour) => {
                         return <Card key={tour.id} {...tour} removeTour={removeTour}></Card>
